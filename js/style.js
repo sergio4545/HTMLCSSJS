39185836ffc60a4653a12cef54231137909d85c7
Serhii <s555e555r555@gmail.com>
2023-04-11 12:32:02 +0300

    proceeding to JS to process the data in the input

--- js/style.js
+++ js/style.js
@@ -1,1 +1,18 @@
 "use strict";
+const price = {
+    copy: {
+        a4black: 5,
+        a4color: 12,
+        a3black: 10,
+        a3clor: 24
+    },
+    photo: {
+        photo10x15: [10, 8, 7],
+        photo13x18: [20, 15, 10],
+        photo15x21: [25, 20, 15],
+        photo21x30: [35, 30, 25]
+    },
+    other: {
+        lamination: [40, 70]
+    }
+}
