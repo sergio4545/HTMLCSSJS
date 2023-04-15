fb21667ab542683b7c4cf823f98cc0479edd832d
Serhii <s555e555r555@gmail.com>
2023-04-15 13:47:36 +0300

    start work I create a class to form blocks

--- js/tasks.js
+++ js/tasks.js
@@ -1,1 +1,11 @@
 "use strickt";
+const newTasksBlock = document.querySelector('.new-tasks__block');
+
+document.querySelector(".new-tasks").addEventListener('click', () => {
+    newTasksBlock.classList.remove('ds-none');
+
+});
+
+document.querySelector('.finish-tasks').addEventListener('click', () => {
+    newTasksBlock.classList.add('ds-none');
+});
